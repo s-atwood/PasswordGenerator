a7f0c31ab25553545718904f5f0500407b5a49fb
Shawn Atwood <sdatwood4@gmail.com>
2024-08-06 03:54:06 -0400

    add error handling for random_indices function

--- flaskr/random_api/random_indices.py
+++ flaskr/random_api/random_indices.py
@@ -1,7 +1,13 @@
-import requests
-import json
+import requests, json, logging
+
+logging.basicConfig(level=logging.INFO)
 
 def get_random_indices(api_key, count, min_index, max_index):
+    if not isinstance(count, int) or count <= 0:
+        raise ValueError("Count must be a positive integer.")
+    if min_index >= max_index:
+        raise ValueError("Min index must be less than max index")
+
     raw_data = {
         "jsonrpc": "2.0",
         "method": "generateIntegers",
@@ -18,6 +24,27 @@ def get_random_indices(api_key, count, min_index, max_index):
     headers = {'Content-type': 'application/json'}
     data = json.dumps(raw_data)
 
-    response = requests.post('https://api.random.org/json-rpc/2/invoke', data=data, headers=headers)
-    random_indices = response.json().get('result', {}).get('random', {}).get('data', [])
+    try:
+        response = requests.post('https://api.random.org/json-rpc/2/invoke', data=data, headers=headers)
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as errh:
+        logging.error(f'HTTP Error: {errh}')
+        return []
+    except requests.exceptions.ConnectionError as errc:
+        logging.error(f'Error ConnectiongL {errc}')
+        return []
+    except requests.exceptions.Timeout as errt:
+        logging.error(f'Timeout Error: {errt}')
+        return []
+    except requests.exceptions.RequestException as err:
+        logging.error(f'Something unexpected happened: {err}')
+        return []
+
+
+    try:
+        random_indices = response.json().get('result', {}).get('random', {}).get('data', [])
+    except Exception as e:
+        logging.error(f'Failed to parse JSON: {e}')
+        return []
+    
     return random_indices
