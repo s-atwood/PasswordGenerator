b27fb2ca8b37e3f811b7042a46ab6f48a5fa287b
Shawn Atwood <sdatwood4@gmail.com>
2024-08-07 04:20:15 -0400

    create react useEffect to hook to flask server

--- flask-server/flaskr/__init__.py
+++ flask-server/flaskr/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
+from flask_caching import Cache
 from dotenv import load_dotenv
 from pathlib import Path
 from .passphrase.generate_passphrase import generate_passphrase
@@ -12,9 +14,13 @@ FILE_PATH = Path('english_corpus.json')
 
 
 def create_app(test_config=None):
+    cache = Cache(config={'CACHE_TYPE': 'simple'})
     app = Flask(__name__, instance_relative_config=True)
+    cache.init_app(app)
+    CORS(app)
     app.config.from_mapping(
         SECRET_KEY='dev',
+        CACHE_DEFAULT_TIMEOUT = 60 * 15,
         DATABASE=os.path.join(app.instance_path,'flaskr.sqlite'),
     )
 
@@ -32,11 +38,15 @@ def create_app(test_config=None):
         pass
 
     @app.route('/', methods=['GET'])
+    @cache.cached(timeout=1)
     def home():
         words = load_words_from_json(FILE_PATH)
         random_indices = get_random_indices(API_KEY, 5, 0, len(words)-1)
         passphrase = generate_passphrase(words, random_indices)
-        return jsonify({'passphrase': passphrase})
+        return jsonify({'Your passphrase': passphrase})
+        # x = {"members": ["member1", "member2"]}
+        # y = {"something", "somethingelse"}
+        # return jsonify(x)
     
     return app
     
